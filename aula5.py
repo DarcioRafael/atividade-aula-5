e53d1ff31d2fa7bc9c68c949f0931e5fd1f8d4de
DarcioRafael <liminhadarcioo@gmail.com>
2024-10-16 20:20:38 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,29 @@
+#exercicio 1
+# idade = int(input(' digite a idade: '))
+
+# if idade >= 18: 
+#     print(f' maior de idade !')
+# else:
+#     print(' menor de idade ! ')
+
+
+#exercicio 2
+
+# n1 = int(input(' digite o primeiro numero: '))
+# n2 = int(input(' digite o segundo numero: '))
+
+# if n1 != n2:
+#     print('o numero é diferente!')
+# else:
+#     print(' o numero é igual!')
+
+#exercicio 3
+
+# n1 = int(input(' digite um numero: '))
+# if n1 >= 0:
+#     print(' o numero é positivo !')
+# elif n1 == 0:
+#     print(' o numero é 0')
+
+# else:
+#     print(' o é negativo !')       
